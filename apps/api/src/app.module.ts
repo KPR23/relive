a5cb8e529d60b4eef2d46c13193bf67402c54123
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-01-13 10:53:33 +0100

    chore: remove old throttler, and add arcjet

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,19 +1,41 @@
 import { Module } from '@nestjs/common';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import {
+  ArcjetGuard,
+  ArcjetModule,
+  detectBot,
+  fixedWindow,
+  shield,
+} from '@arcjet/nest';
 import { TrpcModule } from './trpc/trpc.module';
 import { TodoModule } from './todo/todo.module';
 import { auth } from './auth';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { TestController } from './test/test.controller';
+import { env } from './env';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 10,
-      },
-    ]),
+    ArcjetModule.forRoot({
+      isGlobal: true,
+      key: env.ARCJET_KEY,
+      rules: [
+        shield({ mode: 'LIVE' }),
+        detectBot({
+          mode: 'LIVE',
+          allow: [
+            'CATEGORY:SEARCH_ENGINE',
+            // 'CATEGORY:MONITOR',
+            // 'CATEGORY:PREVIEW',
+          ],
+        }),
+        fixedWindow({
+          mode: 'LIVE',
+          window: '60s',
+          max: 100,
+        }),
+      ],
+    }),
     TrpcModule,
     TodoModule,
     AuthModule.forRoot({ auth }),
@@ -21,8 +43,9 @@ import { AuthModule } from '@thallesp/nestjs-better-auth';
   providers: [
     {
       provide: APP_GUARD,
-      useClass: ThrottlerGuard,
+      useClass: ArcjetGuard,
     },
   ],
+  controllers: [TestController],
 })
 export class AppModule {}
