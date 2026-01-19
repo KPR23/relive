f2eb12b64678ea28e13e861fe4221bd4b119086d
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-01-19 19:28:33 +0100

    build: remove unnecessary packages

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,9 +1,9 @@
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import { env } from '../env.server';
+import { env } from 'src/env.server.js';
 
 const pool = new Pool({
-  connectionString: env.DATABASE_URL!,
+  connectionString: env.DATABASE_URL,
 });
 
 export const db = drizzle(pool);
