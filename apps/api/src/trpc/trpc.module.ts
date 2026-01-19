39e16705ec98ca4abd9f7952e9b8d386420ccc11
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-01-19 22:05:21 +0100

    fix: update vercel.json

--- apps/api/src/trpc/trpc.module.ts
+++ apps/api/src/trpc/trpc.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TRPCModule } from 'nestjs-trpc';
 import { AppContext } from './context.js';
+import { env } from 'src/env.server.js';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
+const isProduction = env.NODE_ENV === 'production';
 @Module({
   imports: [
     TRPCModule.forRoot({
