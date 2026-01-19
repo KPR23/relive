4d3d3219c60c44a64fa2bb12723b65e85f7a657e
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-01-19 22:15:18 +0100

    feat: Establish `api/index.js` as the Vercel serverless function entry point and update the build command.

---
+++ apps/api/api/index.js
@@ -0,0 +1,5 @@
+// This file is used by Vercel to detect the serverless function
+// It points to the compiled main.js in the dist folder
+import { handler } from '../dist/src/main.js';
+
+export default handler;
