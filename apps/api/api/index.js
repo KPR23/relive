3a4965a4f3850fd9307a199cfae24bb13651e4f7
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-01-19 22:30:33 +0100

    chore: Switch Vercel API entry point from `index.js` to `index.ts` and update configuration accordingly.

--- apps/api/api/index.js
+++
@@ -1,5 +0,0 @@
-// This file is used by Vercel to detect the serverless function
-// It points to the compiled main.js in the dist folder
-import { handler } from '../dist/src/main.js';
-
-export default handler;
