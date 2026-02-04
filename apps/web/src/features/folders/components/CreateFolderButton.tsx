2949d62c51561ed97be4436358fcc11555242a50
Kacper Zabłudowski <zabluka_aehit@students.vizja.pl>
2026-02-04 11:57:15 +0100

    style: Add placeholder to folder name input and remove margin from button.

--- apps/web/src/features/folders/components/CreateFolderButton.tsx
+++ apps/web/src/features/folders/components/CreateFolderButton.tsx
@@ -25,11 +25,12 @@ export default function CreateFolderButton({
       <input
         type="text"
         value={name}
+        placeholder="Folder name"
         className="h-12 rounded-full border border-gray-300 bg-gray-200 px-4 py-2 text-black dark:border-gray-700 dark:bg-gray-800 dark:text-white"
         onChange={(e) => setName(e.target.value)}
       />
       <button
-        className="m-4 h-12 cursor-pointer rounded-full bg-yellow-900 p-2 px-4 text-white"
+        className="h-12 cursor-pointer rounded-full bg-yellow-900 p-2 px-4 text-white"
         onClick={(e) => {
           e.preventDefault();
           if (!effectiveParentId) return;
